5c6bccf58706b491b88e1c28cc7a8c1bf15afeb5
Milenco Ieremici <milenco@widgit.com>
2025-02-26 12:48:15 +0000

    Destroy all reviews associated with the deleted restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,11 @@
+
+
 class Restaurant < ApplicationRecord
   CATEGORIES = %w[chinese italian japanese french belgian]
 
+  has_many :reviews, dependent: :destroy
+
   validates :name, :address, :phone_number, :category, presence: true
   validates :category, inclusion: { in: CATEGORIES }
+
 end
