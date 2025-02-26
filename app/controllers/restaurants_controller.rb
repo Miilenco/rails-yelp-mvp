911e72e42502bd830eee887d0816e741855e51bc
Milenco Ieremici <milenco@widgit.com>
2025-02-26 14:52:33 +0000

    Added actions 'index', 'new' and 'create' to 'Restaurants' controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,24 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    if @restaurant
+      redirect_to restaurants_path
+    else
+      redirect_to restaurants_path(@restaurant), status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
