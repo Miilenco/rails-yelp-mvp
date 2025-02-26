67bc059a726580bd585e3dfd4d779ceec07c18b5
Milenco Ieremici <milenco@widgit.com>
2025-02-26 14:27:23 +0000

    Populated seed file with 5 restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,7 +5,7 @@ class Restaurant < ApplicationRecord
 
   has_many :reviews, dependent: :destroy
 
-  validates :name, :address, :phone_number, :category, presence: true
+  validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: CATEGORIES }
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Adding restaurants to the database'
+puts "Creating 'Colaba' restaurant"
+colaba = Restaurant.create!(name: 'Colaba', address: '279 W End Lane', category: 'french')
+puts "Created 'Colaba' restaurant"
+
+puts "Creating 'Comfort Burger' restaurant"
+comfort_burger = Restaurant.create!(name: 'Comfort Burger', address: '218 W End Lane', category: 'belgian')
+puts "Created 'Comfort Burger' restaurant"
+
+puts "Creating 'Queen of Sheeba' restaurant"
+queen_of_sheeba = Restaurant.create!(name: 'Queen of Sheeba', address: '12 Fortess Road', category: 'chinese')
+puts "Created 'Queen of Sheeba' restaurant"
+
+puts "Creating 'Haku Kitchin' restaurant"
+haku_kitchin = Restaurant.create!(name: 'Haku Kitchin', address: '118 Fortress Road', category: 'japanese')
+puts "Created 'Haku Kitchin' restaurant"
+
+puts "Creating 'Gloria' restaurant"
+gloria = Restaurant.create!(name: 'Gloria', address: '54-56 Great Eastern Street', category: 'italian')
+puts "Created 'Gloria' restaurant"
+puts 'Finished seeding'
