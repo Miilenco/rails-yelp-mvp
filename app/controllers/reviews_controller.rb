855296cf98ae1f849a0a64c651f3f7e01939f219
Milenco Ieremici <milenco@widgit.com>
2025-02-26 16:15:48 +0000

    Added action 'index' to 'Reviews' controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,12 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[index]
+
+  def index
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 end
